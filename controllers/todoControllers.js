d65ad0349c779449f67cbb939e647538432adeee
Bhavik Joshi <bhavikjoshi29@gmail.com>
2017-04-04 03:14:19 +0530

    Added POST and DELETE Route

--- controllers/todoControllers.js
+++ controllers/todoControllers.js
@@ -1,15 +1,21 @@
+var bodyParser = require('body-parser');
+var data = [{item: 'Complete 1'}, {item: 'Complete 2'}, {item: 'Complete 3'}];
+var urlencoded = bodyParser.urlencoded({extended: false});
+
 module.exports = (app) => {
     
     app.get('/todo', (req,res) => {
-        res.render('todo');
+        res.render('todo', {todos : data});
     });
 
-    app.post('/todo', (req,res) => {
-
+    app.post('/todo', urlencoded , (req,res) => {
+        data.push(req.body);
+        res.json(data);
     });
 
-    app.delete('/todo', (req,res) => {
-
+    app.delete('/todo/:item', (req,res) => {
+        data = data.filter((todo) => todo.item.replace(/ /g, '-') !== req.params.item);
+        res.json(data);
     });
 
 }
