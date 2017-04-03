37676a7b59506995d2afe98656828814bb56c81c
Bhavik Joshi <bhavikjoshi29@gmail.com>
2017-04-04 04:48:20 +0530

    Completed Routes to Mongoose on Heroku

--- app.js
+++ app.js
@@ -1,28 +1,5 @@
 var express = require('express');
 var todoControllers = require('./controllers/todoControllers');
-var mongoose = require('mongoose');
-
-//connect to Database
-mongoose.connect('mongodb://test_user:testUser1@ds149820.mlab.com:49820/heroku_rqp2p7kf' || 'mongodb://localhost:27017/todo', (err, db) => {
-    if(err) {
-        return console.log("Unable to connect to MongoDB Server.");
-    }
-    console.log("Connected to MongoDB Server.");
-});
-
-//Database Schema
-var todoSchema = new mongoose.Schema({
-    item : String
-});
-
-var Todo = mongoose.model('Todo',todoSchema);
-
-var itemOne = Todo({item : 'Buy Flowers'}).save((err) => {
-    if(err) {
-        return console.log("There was some problem with the Server.");
-    }
-    console.log("Successfull");
-});
 
 const port = process.env.PORT || 3000;
 var app = express();
--- controllers/todoControllers.js
+++ controllers/todoControllers.js
@@ -1,21 +1,51 @@
 var bodyParser = require('body-parser');
-var data = [{item: 'Complete 1'}, {item: 'Complete 2'}, {item: 'Complete 3'}];
+//var data = [{item: 'Complete 1'}, {item: 'Complete 2'}, {item: 'Complete 3'}];
 var urlencoded = bodyParser.urlencoded({extended: false});
+
+var mongoose = require('mongoose');
+
+//connect to Database
+mongoose.connect('mongodb://test_user:testUser1@ds149820.mlab.com:49820/heroku_rqp2p7kf' || 'mongodb://localhost:27017/todo', (err, db) => {
+    if(err) {
+        return console.log("Unable to connect to MongoDB Server.");
+    }
+    console.log("Connected to MongoDB Server.");
+});
+
+//Database Schema
+var todoSchema = new mongoose.Schema({
+    item : String
+});
+
+var Todo = mongoose.model('Todo',todoSchema);
 
 module.exports = (app) => {
     
     app.get('/todo', (req,res) => {
-        res.render('todo', {todos : data});
+        Todo.find({},(err, data) => {
+            if(err) {
+                return console.log("Got Some Error");
+            }
+            res.render('todo', {todos : data});
+        });
     });
 
     app.post('/todo', urlencoded , (req,res) => {
-        data.push(req.body);
-        res.json(data);
+        var newTodo = Todo(req.body).save((err, data) => {
+            if(err) {
+                return console.log("Got Some Error");
+            }
+            res.json(data);
+        });
     });
 
     app.delete('/todo/:item', (req,res) => {
-        data = data.filter((todo) => todo.item.replace(/ /g, '-') !== req.params.item);
-        res.json(data);
+        Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove((err,data) => {
+            if(err) {
+                return console.log("Got Some Error");
+            }
+            res.json(data);
+        });
     });
 
 }
